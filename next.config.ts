ed91f57e7ec45bea77868cf400a0c4929cf95eaf
johyukrae <huu000008@gmail.com>
2025-04-18 13:23:33 +0900

    feat: notion api 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // experimental: {
+  //   typedRoutes: true,
+  // },
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'picsum.photos',
+      },
+      {
+        hostname: 'images.unsplash.com',
+      },
+      {
+        hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
